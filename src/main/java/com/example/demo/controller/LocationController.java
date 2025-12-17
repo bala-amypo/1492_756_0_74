a1cc2f54e69a49408090e565f9acd279c0a2a9eb
bala-amypo <bala@amypo.in>
2025-12-17 08:09:06 +0000

    User commit

--- src/main/java/com/example/demo/controller/LocationController.java
+++ src/main/java/com/example/demo/controller/LocationController.java
@@ -2,7 +2,13 @@
 public class LocationController{
     @Autowired
     LocationService locationservice;
-    @PostMapping("/addstudent")
-    public LocationEntity add()
+    @PostMapping("/addlocation")
+    public LocationEntity add(@RequestBody LocationEntity le){
+        return locationservice.createlocation(le);
+    }
+    @GetMapping("/showlocation")
+    public List<LocationEntity> show(){
+        return locationservice.getalllocation();
+    }
 
 }
